9782cb9c819d1cc80d8c344954f0d45388a212d7
Jorge Navarro <jorgetbg@hotmail.com>
2024-09-24 19:06:36 -0300

    Aula 08

---
+++ meu_pacote/utils/calculos.py
@@ -0,0 +1,10 @@
+# calculos.py
+
+# Isto é um módulo
+# módulo é um arquivo contendo funções para importação em outro arquivo
+
+def somar(a, b):
+    return a + b
+
+def subtrair(a, b):
+    return a + b
---
+++ principal.py
@@ -0,0 +1,5 @@
+#    pacote/subpacote        módulo
+from meu_pacote.utils import * 
+
+r = somar(1,2)
+print(r)
